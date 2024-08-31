02c719bcda97c5e15beb7add458979f35046ada5
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2024-08-31 02:40:42 +0200

    removed some testing stuff

--- Bolt/src/Platform/GlfwWindow.cpp
+++ Bolt/src/Platform/GlfwWindow.cpp
@@ -116,9 +116,6 @@ namespace Bolt {
 				BT_INFO_CORE("GLFW Debugger Initialized.");
 			}
 		}
-
-		glBindVertexArray(20);
-
 		#endif
 
 		// Enabling vsync
@@ -167,7 +164,6 @@ namespace Bolt {
 		glClearColor(0.f, 0.f, 0.f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
-
 
 	bool GlfwWindow::shouldWindowClose() const { return glfwWindowShouldClose(this->m_context); }
 
