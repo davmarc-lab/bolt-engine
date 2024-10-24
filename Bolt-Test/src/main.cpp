53a677f227d67d3bdcde1541dfc05ea97375dc08
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2024-10-24 16:18:16 +0200

    little changes

--- Bolt-Test/src/main.cpp
+++ Bolt-Test/src/main.cpp
@@ -4,45 +4,47 @@
 using namespace bolt;
 
 int main(int argc, char *argv[]) {
+	std::cout << "Application started\n";
+
 	WindowProperties properties{};
 	properties.maximized = false;
 	properties.vsync = true;
 	properties.backgroundColor = vec4(0.3, 0.3, 0.3, 1);
-    properties.cull = {true, GL_BACK};
-    properties.depth = {true, true, GL_LESS};
+	properties.cull = {true, GL_BACK};
+	properties.depth = {true, true, GL_LESS};
 
 	ApplicationSetting settings{};
 	settings.type = scene::SCENE_3D;
 	settings.name = "Testing";
 	settings.dimension = {1600, 900};
 	settings.properties = properties;
-    settings.defaultCameraMovement = true;
+	settings.defaultCameraMovement = true;
 
 	const auto app = CreateUnique<Application>(settings);
 	const auto ls = LayerStack::instance();
 
 	const auto w = CreateShared<Window>(settings);
-    // it needs to be attached due to UniforBuffer usage
-    w->onAttach();
+	// it needs to be attached due to UniforBuffer usage
+	w->onAttach();
 	ls->addCustomLayer(w);
 
-    const auto scene = Scene::instance();
-    ls->addCustomLayer(CreateShared<SceneLayer>());
+	const auto scene = Scene::instance();
+	ls->addCustomLayer(CreateShared<SceneLayer>());
 
 	UniformBuffer ub = UniformBuffer();
 	ub.onAttach();
 	ub.setup(sizeof(mat4), 0);
-    auto proj = Application::getProjectionMatrix();
+	auto proj = Application::getProjectionMatrix();
 	ub.update(0, sizeof(mat4), value_ptr(proj));
-    EventDispatcher::instance()->subscribe(events::shader::ShaderProjectionChanged, [&proj, &ub](auto &&p) {
+	EventDispatcher::instance()->subscribe(events::shader::ShaderProjectionChanged, [&proj, &ub](auto &&p) {
 		ub.update(0, sizeof(mat4), value_ptr(proj));
 	});
 
-    EntityManager::instance()->subscribeEventCallbacks();
+	EntityManager::instance()->subscribeEventCallbacks();
 
-    Application::enableImGui();
-    const auto ig = CreateShared<ImGuiLayer>(w);
-    ls->addCustomLayer(ig);
+	Application::enableImGui();
+	const auto ig = CreateShared<ImGuiLayer>(w);
+	ls->addCustomLayer(ig);
 
 	ls->addCustomLayer(CreateShared<ImGuiDockSpace>());
 	ls->addCustomLayer(CreateShared<ImGuiEntityTree>());
@@ -51,5 +53,6 @@ int main(int argc, char *argv[]) {
 	ls->addCustomLayer(CreateShared<ImGuiProperties>());
 
 	app->run();
-}
 
+	std::cout << "Application closed\n";
+}
